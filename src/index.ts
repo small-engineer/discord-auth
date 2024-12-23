756317a8f948c343532b57797d1360d7ccce6f12
small-engineer <wakamiya626@gmail.com>
2024-12-23 17:05:56 +0900

    add

--- src/index.ts
+++ src/index.ts
@@ -15,15 +15,13 @@ const client = new Client({
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMembers,
     GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent, // メッセージ本文やメンションを読み取る場合必須
+    GatewayIntentBits.MessageContent,
   ],
 });
 
 // イベントを登録
 client.once("ready", () => ready(client));
 client.on("interactionCreate", (interaction) => interactionCreate(interaction));
-
-// [追加] messageCreateイベント
 client.on("messageCreate", (message) => messageCreate(message));
 
 // Bot起動
